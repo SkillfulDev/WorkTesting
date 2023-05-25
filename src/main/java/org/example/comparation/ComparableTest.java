74ebc84d4d194e5bb1f9c67227594d9e5a65186c
skillfuldev <chernonog.evgeniy@gmail.com>
2023-05-25 13:40:00 +0300

    add_Middle_Comparable

--- src/main/java/org/example/comparation/ComparableTest.java
+++ src/main/java/org/example/comparation/ComparableTest.java
@@ -1,15 +1,34 @@
 package org.example.comparation;
 
+import lombok.*;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class ComparableTest {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>(List.of("Zaur", "Zaur", "Svetlana", "Kate", "Bob", "Ada"));
-        System.out.println(list);
-        Collections.sort(list);
-        System.out.println(list);
+        List<Employee> employees = new ArrayList<>(List.of(
+                Employee.builder().id(100).name("Sergey").surname("Tregulov").salary(1000).build(),
+                Employee.builder().id(300).name("Kate").surname("Smirnova").salary(5000).build(),
+                Employee.builder().id(200).name("Sam").surname("Smith").salary(9000).build()));
+
+        System.out.println(employees);
+        Collections.sort(employees);
+
+
     }
 
+}
+
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+class Employee {
+    int id;
+    String name;
+    String surname;
+    int salary;
 }
