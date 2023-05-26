e3578bcfd0971e380302c108710f824421116ca2
skillfuldev <chernonog.evgeniy@gmail.com>
2023-05-26 11:35:03 +0300

    add_Middle_Comparator

--- src/main/java/org/example/comparation/ComparableTest.java
+++ src/main/java/org/example/comparation/ComparableTest.java
@@ -10,12 +10,16 @@ import java.util.List;
 public class ComparableTest {
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>(List.of(
-                Employee.builder().id(100).name("Sergey").surname("Tregulov").salary(1000).build(),
+                Employee.builder().id(1000).name("Kate").surname("Tregulov").salary(1000).build(),
                 Employee.builder().id(300).name("Kate").surname("Smirnova").salary(5000).build(),
                 Employee.builder().id(200).name("Sam").surname("Smith").salary(9000).build()));
 
-        employees.stream().sorted(Comparator.comparing(Employee::getName)).th
-//        System.out.println(employees);
+
+
+     Comparable(Employee.builder().id(1000).name("Kate").surname("Tregulov").salary(1000).build());
+        employees.stream().sorted(Comparator.comparing(Employee::getName)
+                .thenComparing(Employee::getId))
+                .forEach(System.out::println);
 
     }
  }
@@ -25,9 +29,17 @@ public class ComparableTest {
 @Getter
 @ToString
 @Builder
-class Employee {
+class Employee implements Comparable<Employee>{
     int id;
     String name;
     String surname;
     int salary;
+
+    @Override
+    public int compareTo(Employee o) {
+
+        return this.getId()-o.getId();
+    }
+
+
 }
