997b688ffba414391e0795b252f1a11ba2e57941
skillfuldev <chernonog.evgeniy@gmail.com>
2023-05-29 20:41:24 +0300

    add_BinarySearch

--- src/main/java/org/example/collection/BinarySearch.java
+++ src/main/java/org/example/collection/BinarySearch.java
@@ -1,12 +1,16 @@
 package org.example.collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BinarySearch {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(List.of(-4,5,-44,324,423,-546,7,8324,324));
         System.out.println(list);
-        list.stream().sorted().forEach(System.out::println);
+        Collections.sort(list);
+        System.out.println("After Collections "+ list);
+        System.out.println(Collections.binarySearch(list, 423));
+
     }
 }
