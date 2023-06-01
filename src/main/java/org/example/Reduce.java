69b3f0da89468e29b6045687dbd8bde93db46666
skillfuldev <chernonog.evgeniy@gmail.com>
2023-06-01 20:44:44 +0300

    add_CorrectReduce

--- src/main/java/org/example/Reduce.java
+++ src/main/java/org/example/Reduce.java
@@ -7,23 +7,6 @@ import java.util.Optional;
 public class Reduce {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(2);
-        numbers.add(1);
-        numbers.add(1);
-        numbers.add(2);
-
-
-
-        Optional<Integer> num = numbers.stream().reduce((a, b) -> a * b);
-
-        if (num.isPresent()) {
-            System.out.println(num.get());
-        } else {
-            System.out.println("error");
-        }
 
     }
 
