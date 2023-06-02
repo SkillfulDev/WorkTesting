1bfeddbfea982f8e9e8e1561df902c73b058efb5
skillfuldev <chernonog.evgeniy@gmail.com>
2023-06-02 11:51:17 +0300

    add_Stack

---
+++ src/main/java/org/example/collection/StackEx.java
@@ -0,0 +1,10 @@
+package org.example.collection;
+
+import java.util.Stack;
+
+public class StackEx {
+    public static void main(String[] args) {
+        Stack<String> ex = new Stack<>();
+
+    }
+}
