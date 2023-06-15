ab154244aec15c0af35d886d840b70126513786d
skillfuldev <chernonog.evgeniy@gmail.com>
2023-06-15 15:42:37 +0300

    add_HashSet

---
+++ src/main/java/org/example/collection/map_interface/HashSetEx.java
@@ -0,0 +1,22 @@
+package org.example.collection.map_interface;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+
+        set.add("set");
+        set.add("set");
+        set.add("set6");
+        set.add("set");
+        set.add("set7");
+        set.add("set");
+        set.add("set");
+        set.add("set2");
+        set.add("set5");
+
+        System.out.println(set);
+    }
+}
