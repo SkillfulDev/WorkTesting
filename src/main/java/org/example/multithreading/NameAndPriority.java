e0ee261835b6ed2ce8ec6bc4f6c092d45f51df6e
skillfuldev <chernonog.evgeniy@gmail.com>
2023-06-30 13:54:42 +0300

    add_Volatile

--- src/main/java/org/example/multithreading/NameAndPriority.java
+++ src/main/java/org/example/multithreading/NameAndPriority.java
@@ -1,45 +1,31 @@
 package org.example.multithreading;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class NameAndPriority implements Runnable {
+class Test extends Thread {
+   volatile boolean running = true;
+
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+        long count = 0;
+        while (running) {
+            count++;
         }
+        System.out.println("End of programm, and number of count= " + count);
     }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Test thread = new Test();
+
+        System.out.println("StartingProgram");
+        thread.start();
+        Thread.sleep(3000);
+        thread.running= false;
+        System.out.println("End main Thread");
+
+
+    }
+
+
 }
 
-class Test {
-    public static void main(String[] args) {
-        Thread thread = new Thread(new NameAndPriority());
-        Thread thread1 = new Thread(new NameAndPriority());
-        Thread thread2 = new Thread(new NameAndPriority());
-        Thread thread3 = new Thread(new NameAndPriority());
-        Thread thread4 = new Thread(new NameAndPriority());
-        Thread thread5 = new Thread(new NameAndPriority());
-        thread1.setName("My thread");
-        thread4.setPriority(Thread.MAX_PRIORITY);
-
-
-        List<Thread> threads = new ArrayList<>();
-
-        threads.add(thread);
-        threads.add(thread1);
-        threads.add(thread2);
-        threads.add(thread3);
-        threads.add(thread4);
-        threads.add(thread5);
-
-
-
-        for (Thread thread6 : threads) {
-            thread6.start();
-            System.out.println(thread6.getName()+"$$$$$$"+thread6.getPriority());
-
-            System.out.println(thread6.getPriority());
-        }
-    }
-}
