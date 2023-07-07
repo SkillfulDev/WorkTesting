042f4024277955294d610c2db4fbd65a290823d0
skillfuldev <chernonog.evgeniy@gmail.com>
2023-07-07 09:10:13 +0300

    add_deamon

---
+++ src/main/java/org/example/multithreading/Lock1.java
@@ -0,0 +1,14 @@
+package org.example.multithreading;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Lock1 {
+    Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            
+        }
+    }
+}
