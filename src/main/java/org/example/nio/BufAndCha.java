bad8bc91cc56f408b5e34fd889c3a5be27493479
skillfuldev <chernonog.evgeniy@gmail.com>
2023-07-24 13:50:28 +0300

    add_Channel_Buff

--- src/main/java/org/example/nio/BufAndCha.java
+++ src/main/java/org/example/nio/BufAndCha.java
@@ -1,7 +1,30 @@
 package org.example.nio;
 
+import java.io.*;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class BufAndCha {
     public static void main(String[] args) {
+        try (RandomAccessFile file = new RandomAccessFile("some.txt", "rw");
+             FileChannel channel = file.getChannel()) {
+            ByteBuffer buffer = ByteBuffer.allocate(250);
+            StringBuilder builder = new StringBuilder();
+            int byteCounter = channel.read(buffer);
+            System.out.println(byteCounter);
+            buffer.flip();
+            while (buffer.hasRemaining()) {
+                builder.append((char) buffer.get());
+
+            }
+            System.out.println(builder);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
